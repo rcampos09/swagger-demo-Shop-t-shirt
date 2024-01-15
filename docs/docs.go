4513b83aec0db57d8a5cf67c9c1e031c2190f808
rcampos09 <rcampos.tapia@gmail.com>
2024-01-15 00:44:20 -0300

    Update Generate swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -341,7 +341,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://swagger-demo-shop-t-shirt.onrender.com/",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "APIs Swagger-Shop-t-Shirt",
